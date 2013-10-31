18190144607efb9c81add3382c6aa17daab49b9f
Jaakko Lindvall <verkel@iki.fi>
2013-10-31 22:54:32 +0200

    Allow special chars in custom roller nick

--- NoppaBot/src/noppabot/NoppaBot.java
+++ NoppaBot/src/noppabot/NoppaBot.java
@@ -73,7 +73,7 @@ public class NoppaBot extends PircBot implements INoppaBot {
 	private enum State { NORMAL, ROLL_PERIOD, SETTLE_TIE };
 	
 	public static final Pattern dicePattern = Pattern.compile("(?:.*\\s)?!?d([0-9]+)(?:\\s.*)?");
-	public static final Pattern dicePatternWithCustomRoller = Pattern.compile("(?:.*\\s)?!?d([0-9]+) ([\\w]+)");
+	public static final Pattern dicePatternWithCustomRoller = Pattern.compile("(?:.*\\s)?!?d([0-9]+) ([^\\s]+)");
 	
 	private Map<String, Random> randoms = new HashMap<String, Random>();
 	private Random commonRandom = new Random();
