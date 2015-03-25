40612632c3b4ddc1c461caa163f2310ab3ae1b4a
Jaakko Lindvall <jaakko.lindvall@kirjastot.fi>
2015-03-25 18:45:33 +0200

    Fix adversary random unevenness

--- NoppaBot/src/adversary/AdversaryRandom.java
+++ NoppaBot/src/adversary/AdversaryRandom.java
@@ -10,7 +10,7 @@ public class AdversaryRandom extends PeekableRandom {
 
 	@Override
 	protected int doIntRoll(int sides) {
-		int val = super.doIntRoll(sides);
+		double val = random.nextDouble() * 100d + 1d;
 		int modified = (int)Math.round(Math.sqrt(sides * val));
 //		System.out.printf("transform %d -> %d\n", val, modified);
 		return modified;
